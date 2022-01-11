662286240059f0ac122b65729c908e3628398a50
tgiannotta <t.giannotta92@gmail.com>
2022-01-11 15:58:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+/* import HelloWorld from "./components/HelloWorld.vue"; */
+
+export default {
+  name: "App",
+  components: {
+    
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  
+}
+</style>
