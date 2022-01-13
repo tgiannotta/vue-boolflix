5a4d1646f09e227c6053931e0d0bd63226b32971
tgiannotta <t.giannotta92@gmail.com>
2022-01-13 10:35:51 +0100

    End Header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
-   
+    <Header />
   </div>
 </template>
 
 <script>
-/* import HelloWorld from "./components/HelloWorld.vue"; */
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    
+    Header,
   },
 };
 </script>
 
 <style lang="scss">
+@import "./style/general.scss";
 #app {
   
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header>
+        <div class="cerca">
+            <input v-model="ricercaUtente" placeholder="Inserisci il nome del film che vuoi cercare " type="text">
+
+            <button @click="$emit('getResult', ricercaUtente)">Cerca</button>
+       
+        </div>
+    </header>
+    
+</template>
+
+
+<script>
+export default {
+    name: 'Header',
+    data: function(){
+        return {
+            ricercaUtente: ''
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    header{
+        height: 100px;
+        background-color: black;
+    
+        .cerca{
+                text-align: right;
+                 line-height: 100px;
+                input{
+                    height: 30px;
+                    width: 300px;
+                    margin-right: 30px;
+                   
+                }
+                button{
+                    height: 30px;
+                    padding: 0 20px;
+                    margin-left: 30px;
+                    font-size: 15px;
+                    border-radius: 10%;
+                    margin-right: 50px;
+                }
+        }
+    }
+</style>
